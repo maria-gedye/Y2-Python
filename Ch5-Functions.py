752f9087bf3666544452bcd9187ef0869cdb42c4
maria-gedye <mariagedye@gmail.com>
2022-03-14 12:35:04 +1300

    exercise 11 math quiz

--- Ch5-Functions.py
+++ Ch5-Functions.py
@@ -1,4 +1,5 @@
 # Chapter 5 Exercises 1, 6, 11, 14, 16, 18
+import random
 
 # Ex1 Kilometer Converter
 
@@ -11,24 +12,43 @@
 # m = km_to_miles()
 # print "Miles: ", m, "m"
 
+
+
 # Ex6 Calories from fat and carbohydrates
-print("Calorie calculator")
-calf = 0
-calc = 0
+# print("Calorie calculator")
+# calf = 0
+# calc = 0
+#
+#
+# def cal_from_fat(cf):
+#     fat = input("Enter grams of fat: ")
+#     cf = fat * 9
+#     return cf
+#
+#
+# def cal_from_carb(cc):
+#     carb = input("Enter grams of carbohydrate:  ")
+#     cc = carb * 4
+#     return cc
+#
+#
+# calf = cal_from_fat(calf)
+# calc = cal_from_carb(calc)
+# print "Total calories from fat: ", calf, "\nTotal calories from carbohydrate: ", calc
 
 
-def cal_from_fat(cf):
-    fat = input("Enter grams of fat: ")
-    cf = fat * 9
-    return cf
+
+# Ex11 Math Quiz
+
+def quiz(n1, n2):
+    print "\t", n1, "\n+\t", n2, "\n="
+    answer = input("What is the answer?  ")
+    if answer == (n1 + n2):
+        print("Correct!")
+    else:
+        print "incorrect. Answer is: ", n1 + n2
 
 
-def cal_from_carb(cc):
-    carb = input("Enter grams of carbohydrate:  ")
-    cc = carb * 4
-    return cc
-
-
-calf = cal_from_fat(calf)
-calc = cal_from_carb(calc)
-print "Total calories from fat: ", calf, "\nTotal calories from carbohydrate: ", calc
+num1 = random.randint(1, 999)
+num2 = random.randint(1, 999)
+quiz(num1, num2)
